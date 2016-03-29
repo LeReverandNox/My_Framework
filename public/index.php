42835f14e0eee9050642b80dba6b9344839b61b0
laidet_r <rodolphe.laidet@epitech.eu>
2016-03-29 12:33:59 +0200

    Première partie du bootstrap effectuée, on inclut le controller appelé par le GET page. C'est tres basique, mais ca colle au sujet

--- public/index.php
+++ public/index.php
@@ -1,1 +1,8 @@
-<?php phpinfo(); ?>
+<?php
+    require_once("../lib/Tools.php");
+
+    if (false !== Tools::getParam("page"))
+    {
+        $controller = Tools::getParam("page");
+        require_once("../app/controllers/" .  ucfirst($controller) . "Controller.php");
+    }
