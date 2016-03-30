b0ee981ac7d6cb4dd1ce1f12e9f956404fa6019c
laidet_r <rodolphe.laidet@epitech.eu>
2016-03-30 10:56:32 +0200

    Creation d'un controller UserController pour tester, avec 2 methodes, Ca fonctionne le routing ! lol

---
+++ app/controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+    namespace app\controllers
+    {
+        class UserController
+        {
+            public function connexionAction()
+            {
+                echo __FUNCTION__;
+            }
+
+            public function inscriptionAction()
+            {
+                echo __FUNCTION__;
+            }
+        }
+    }
