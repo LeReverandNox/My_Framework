7927a0930f7c21744b4dfca253e1386ddb8ab1e8
laidet_r <rodolphe.laidet@epitech.eu>
2016-03-30 12:27:01 +0200

    Ecriture de la fonction registerAutoload avec l'autoloader, on replace les \ en \, on test si le fichier existe et on le require apres. Voila un beau autoloader

--- lib/LPDR/Core.php
+++ lib/LPDR/Core.php
@@ -5,7 +5,7 @@
         {
             public static function run()
             {
-                require_once("Tools.php");
+                self::registerAutoload();
 
                 if (false !== Tools::getParam("page"))
                 {
@@ -21,6 +21,24 @@
                         $controller->$actionName();
                     }
                 }
+            }
+
+            public static function registerAutoload()
+            {
+                spl_autoload_register(
+                    function ($class) {
+                        $class = str_replace("\\", "/", $class);
+
+                        if (true === file_exists("../" . $class . ".php"))
+                        {
+                            require_once("../" . $class . ".php");
+                        }
+                        if (true === file_exists("../lib/" . $class . ".php"))
+                        {
+                            require_once("../lib/" . $class . ".php");
+                        }
+                    }
+                );
             }
         }
     }
