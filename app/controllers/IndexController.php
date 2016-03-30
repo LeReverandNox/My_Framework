3322ecbb6e1e26119404ae0817692dd8a9588b1c
laidet_r <rodolphe.laidet@epitech.eu>
2016-03-30 23:19:56 +0200

    Ecriture du model terminée, tous les attribus necessaires sont present, tous privés et statiques. Le constructeur, si la connexion n'est pas déjà etablie, en lance une nouvelle, sois sur le socke, sois sur l'hostname+port, avec un try/catch. Test de ca dans IndexController

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,11 +2,13 @@
     namespace app\controllers
     {
         use LPDR\Controller;
+        use app\models\UserTable;
 
         class IndexController extends Controller
         {
             public function indexAction()
             {
+                $u = new UserTable();
                 $this->render(__CLASS__ . ":" . "index", array("username" => "Marie"));
             }
 
--- lib/LPDR/Model.php
+++ lib/LPDR/Model.php
@@ -3,18 +3,88 @@
     {
         abstract class Model
         {
-            private static $host;
-            private static $port;
-            private static $socket;
-            private static $username;
-            private static $password;
+            private static $hostname = "127.0.0.1";
+            private static $port = "3306" ;
+            private static $socket = null;
+            private static $username = "laidet_r";
+            private static $password = "94Wq6E3Q8DzbSWR401NO";
+            private static $dbname = "laidet_r_my_framework";
 
-            private static $pdo;
+            protected static $pdo;
+
+            public static function getHostname()
+            {
+                return self::$host;
+            }
+            public static function setHostname($hostname)
+            {
+                self::$hostname = $hostname;
+            }
+
+            public static function getPort()
+            {
+                return self::$port;
+            }
+            public static function setPort($port)
+            {
+                self::$port = $port;
+            }
+
+            public static function getSocket()
+            {
+                return self::$socket;
+            }
+            public static function setSocket($socket)
+            {
+                self::$socket = $socket;
+            }
+
+            public static function getUsername()
+            {
+                return self::$username;
+            }
+            public static function setUsername($username)
+            {
+                self::$username = $username;
+            }
+
+            public static function getPassword()
+            {
+                return self::$password;
+            }
+            public static function setPassword($password)
+            {
+                self::$password = $password;
+            }
+
+
+
+            public static function getDbname()
+            {
+                return self::$dbname;
+            }
+            public static function setDbname($dbname)
+            {
+                self::$dbname = $dbname;
+            }
+
 
             public function __construct()
             {
-                if (null === self::$pdo) {
-                    # code...
+                if (null === self::$pdo)
+                {
+                    try {
+                        if (null !== self::$socket)
+                        {
+                            self::$pdo = new \PDO("mysql:dbname=" . self::$dbname . ";unix_socket=" . self::$socket, self::$username, self::$password);
+                        }
+                        else
+                        {
+                            self::$pdo = new \PDO("mysql:dbname=" . self::$dbname . ";host=" . self::$hostname . ";port=" . self::$port, self::$username, self::$password);
+                        }
+                    } catch (PDOException $e) {
+                        echo "Failed to connect to the database : " . $e;
+                    }
                 }
             }
         }
