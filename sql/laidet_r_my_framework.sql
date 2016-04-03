055d7864030c6035c7635d51b15de5702ca4bb70
laidet_r <rodolphe.laidet@epitech.eu>
2016-04-03 20:33:24 +0200

    Dernier export de la DB

--- sql/laidet_r_my_framework.sql
+++ sql/laidet_r_my_framework.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.12deb2+deb8u1build0.15.04.1
+-- version 4.4.13.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Sam 02 Avril 2016 à 22:32
--- Version du serveur :  5.6.28-0ubuntu0.15.04.1
--- Version de PHP :  5.6.4-4ubuntu6.4
+-- Généré le :  Dim 03 Avril 2016 à 20:32
+-- Version du serveur :  5.6.28-0ubuntu0.15.10.1
+-- Version de PHP :  5.6.11-1ubuntu3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `laidet_r_my_framework`
@@ -29,12 +29,12 @@ USE `laidet_r_my_framework`;
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-`user_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `login` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 --
 -- Vider la table avant d'insérer `user`
@@ -46,7 +46,9 @@ TRUNCATE TABLE `user`;
 --
 
 INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `login`, `email`) VALUES
-(1, 'rodolphe', 'laidet', 'bobbycarotte', 'lol@gmail.com');
+(1, 'rodolphe', 'laidet', 'bobbycarotte', 'lol@gmail.com'),
+(13, 'Marie', 'Chu', 'chu_m', 'choupette@gmail.com'),
+(16, 'Allan', 'Guichard', 'guicha_a', 'jackyman@gmail.com');
 
 --
 -- Index pour les tables exportées
@@ -56,7 +58,7 @@ INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `login`, `email`) VALUES
 -- Index pour la table `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -66,7 +68,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
